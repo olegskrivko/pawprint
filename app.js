74d9bb07a4e3d1b711e663c1c8a6902bcbdd3f8f
Olegs <olegs.krivko@gmail.com>
2023-06-23 14:09:58 +0300

    refactor app

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-const i18n = require("i18n");
-const { languageMiddleware } = require("./middleware/middleware");
+// Import required libraries
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const engine = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
-const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user");
+const MongoStore = require("connect-mongo");
 const mongoSanitize = require("express-mongo-sanitize");
+const i18n = require("i18n");
 
-// routes
+// Import middleware and utility
+const { languageMiddleware } = require("./middleware/middleware");
+const ExpressError = require("./utils/ExpressError");
+
+// Import routes
 const userRoutes = require("./routes/userRoutes");
 const aboutRoutes = require("./routes/aboutRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
@@ -24,36 +24,17 @@ const petRoutes = require("./routes/petRoutes");
 const compareRoutes = require("./routes/compareRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const locationRoutes = require("./routes/locationRoutes");
-// const nodemailer = require("nodemailer");
-// const favicon = require("serve-favicon");
-const MongoStore = require("connect-mongo");
-const dbURL = process.env.DB_URL || "mongodb://localhost:27017/pet-finder";
-// const dbURL = "mongodb://localhost:27017/pet-finder";
 
-mongoose.connect(dbURL, () => {
-  console.log("Conected to DB!");
-});
+// Import model
+const User = require("./models/user");
 
-// Set the value of strictQuery explicitly
-mongoose.set("strictQuery", false);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", () => {
-  console.log("Database connected!");
-});
-
+// Create Express app
 const app = express();
 
-// Middleware
-app.engine("ejs", engine);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
-// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-app.use(mongoSanitize());
+// Load environment variables from .env file if not in production
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 // Configure i18n middleware
 i18n.configure({
@@ -66,6 +47,26 @@ i18n.configure({
 });
 app.use(i18n.init);
 
+// Connect to MongoDB
+const dbURL = process.env.DB_URL;
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error: ", error);
+  });
+
+// Set the value of strictQuery explicitly (no longer necessary in recent versions of Mongoose)
+mongoose.set("strictQuery", false);
+
+// Set up the session store
 const secret = process.env.SECRET;
 const sessionConfig = {
   store: MongoStore.create({
@@ -84,19 +85,27 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
+// Set up Express app
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Add the language middleware after the session middleware
-// app.use(languageMiddleware);
-
+// Configure Passport.js authentication
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Make currentUser, success, and error available in all templates
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
@@ -104,8 +113,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes petRoutes userRoutes
-
+// Define routes
 app.use("/auth", userRoutes);
 app.use("/compare", compareRoutes);
 app.use("/pets", petRoutes);
@@ -114,15 +122,12 @@ app.use("/pets/:id/comments", commentRoutes);
 app.use("/services", serviceRoutes);
 app.use("/regions", locationRoutes);
 
+// Home route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// app.all("*", (req, res, next) => {
-//   // next(new ExpressError("Page Not Found", 404));
-//   res.render("pagenotfound");
-// });
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   const { statuscode = 500 } = err;
   if (!err.message) {
@@ -131,8 +136,8 @@ app.use((err, req, res, next) => {
   res.status(statuscode).render("error", { err });
 });
 
+// Start the server
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`Serving on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
