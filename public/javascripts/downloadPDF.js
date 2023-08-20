2dbf072199424b510a958ea62d271a0e3731245d
Olegs <olegs.krivko@gmail.com>
2023-08-20 12:10:44 +0300

    remove comment

--- public/javascripts/downloadPDF.js
+++
@@ -1,4 +0,0 @@
-// const doc = new jsPDF();
-// doc.addImage('https://res.cloudinary.com/dymne7cde/image/upload/v1684082424/PetFinder/azltxnekf0v2he30woz4.jpg', 'JPEG', 10, 10, 50, 50);
-// doc.save('example.pdf');
-// <button onclick="downloadPDF()">Download PDF</button>
